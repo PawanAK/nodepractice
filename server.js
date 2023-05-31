f4e000c8ea5150a260d9fcbf88b18edbbbd431fe
PawanAK <pawanajjark@gmail.com>
2023-06-01 00:27:42 +0530

    listening

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello Iam response");
+  })
+  .listen(8181, "127.0.0.1");
